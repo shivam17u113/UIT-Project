3d3d8df3983ab595a44b8eee703febd580bec2b5
shivam <shivam.p.parve@gmail.com>
2020-06-02 11:16:37 +0530

    final project is implemented

---
+++ NodeJsAssignment/client-side/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    
+  <router-view/>
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  components: {
+    
+  },
+  data() {
+    return {
+      
+    }
+  },
+  created() {
+    
+  },
+}
+</script>
+
+<style>
+
+</style>
---
+++ NodeJsAssignment/client-side/src/components/test.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    
+<h1>shivam parve</h1>
+  </div>
+</template>
+
+<script>
+
+export default {
+  components: {
+    
+  },
+  data() {
+    return {
+      
+    }
+  },
+  created() {
+    
+  },
+}
+</script>
+
+<style>
+
+</style>
